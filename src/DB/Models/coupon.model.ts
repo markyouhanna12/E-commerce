d3f823d241ba2c3664d0c43e8aeaae332a6cea78
markyouhanna12 <markyouhanna1@gmail.com>
2026-07-23 21:06:53 +0300

    feat(coupon): add Coupon model with Mongoose schema for coupon management

---
+++ src/DB/Models/coupon.model.ts
@@ -0,0 +1,118 @@
+import { MongooseModule, Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { ref } from 'process';
+
+export enum CouponType {
+  PERCENTAGE = 'PERCENTAGE',
+  FIXED = 'FIXED',
+}
+
+@Schema({
+  timestamps: true,
+})
+export class Coupon {
+  @Prop({
+    required: true,
+    unique: true,
+    uppercase: true,
+    trim: true,
+  })
+  code!: string;
+
+  @Prop({
+    required: true,
+  })
+  name!: string;
+
+  @Prop({
+    default: '',
+  })
+  description!: string;
+
+  @Prop({
+    enum: CouponType,
+    required: true,
+  })
+  type!: CouponType;
+
+  @Prop({
+    required: true,
+  })
+  value!: number;
+
+  @Prop({
+    default: true,
+  })
+  isActive!: boolean;
+
+  @Prop({
+    required: true,
+  })
+  expiresAt!: Date;
+
+  @Prop({
+    default: 0,
+  })
+  usedCount!: number;
+
+  @Prop({
+    default: 1,
+  })
+  maxUses!: number;
+
+  @Prop({
+    default: 1,
+  })
+  maxUsesPerUser!: number;
+
+  @Prop({
+    default: 0,
+  })
+  minimumPurchase!: number;
+
+  @Prop({
+    default: 0,
+  })
+  maximumDiscount!: number;
+
+  @Prop([
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+    },
+  ])
+  products!: mongoose.Types.ObjectId[];
+
+  @Prop([
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+    },
+  ])
+  categories!: mongoose.Types.ObjectId[];
+
+  @Prop([
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Brand',
+    },
+  ])
+  brands!: mongoose.Types.ObjectId[];
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  })
+  createdBy!: mongoose.Types.ObjectId;
+}
+
+export const CouponSchema = SchemaFactory.createForClass(Coupon);
+
+export type HCouponDocument = HydratedDocument<Coupon>;
+
+export const CartModel = MongooseModule.forFeature([
+  {
+    name: 'Cart',
+    schema: CouponSchema,
+  },
+]);
