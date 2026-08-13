125544842887a0593dbb198a54eefb3f2caa6111
markyouhanna12 <markyouhanna1@gmail.com>
2026-08-13 19:30:53 +0300

    feat(wishlist): enhance Wishlist module with authentication and additional methods

--- src/wishlist/wishlist.controller.ts
+++ src/wishlist/wishlist.controller.ts
@@ -1,7 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { WishlistService } from './wishlist.service';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RoleEnum } from 'src/Common/Enums/user.enums';
 
 @Controller('wishlist')
+@UseGuards(AuthGuard, RolesGuard)
+@Roles(RoleEnum.USER)
 export class WishlistController {
   constructor(private readonly wishlistService: WishlistService) {}
+
+  @Post(':productId')
+  async addProduct(@Req() req: any, @Param('productId') productId: string) {
+    return this.wishlistService.addProduct(req.user.id, productId);
+  }
+
+  @Delete(':productId')
+  async removeProduct(@Req() req: any, @Param('productId') productId: string) {
+    return this.wishlistService.removeProduct(req.user.id, productId);
+  }
+  @Get()
+  async getWishlist(@Req() req: any) {
+    return this.wishlistService.getWishlist(req.user.id);
+  }
+
+  @Get('check/:productId')
+  async checkProduct(@Req() req: any, @Param('productId') productId: string) {
+    return this.wishlistService.checkProduct(req.user.id, productId);
+  }
+
+  @Delete()
+  async clearWishlist(@Req() req: any) {
+    return this.wishlistService.clearWishlist(req.user.id);
+  }
 }
--- src/wishlist/wishlist.module.ts
+++ src/wishlist/wishlist.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { WishlistService } from './wishlist.service';
 import { WishlistController } from './wishlist.controller';
+import { WishlistModel } from 'src/DB/Models/wishlist.model';
+import { ProductModel } from 'src/DB/Models/products.model';
+import { UserModel } from 'src/DB/Models/user.model';
+import { JwtService } from '@nestjs/jwt';
+import { TokenService } from 'src/Common/Tokens/token.service';
 
 @Module({
+  imports: [WishlistModel, ProductModel, UserModel],
   controllers: [WishlistController],
-  providers: [WishlistService],
+  providers: [WishlistService, JwtService, TokenService],
 })
 export class WishlistModule {}
